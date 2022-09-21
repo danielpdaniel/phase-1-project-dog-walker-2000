896e481d207b43b5067d3e8598b5b6a4c8481614
danielpdaniel <artofjquinn@gmail.com>
2022-09-21 13:19:57 -0400

    linked index.js to index.html and added a DOMContentLoaded event listener to index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let newHeader = document.createElement("h2");
+    newHeader.textContent = "hiiiiiii"
+    document.querySelector("body").appendChild(newHeader)
+})
