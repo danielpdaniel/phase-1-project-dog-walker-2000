a01274a6666281950c8ded872038fcf00a9678e0
danielpdaniel <artofjquinn@gmail.com>
2022-09-21 14:22:47 -0400

    fetched a random dog image and added it to the dom

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let newHeader = document.createElement("h2");
     newHeader.textContent = "hiiiiiii"
     document.querySelector("body").appendChild(newHeader)
+
+    const dogUrl = "https://dog.ceo/api/breeds/image/random/"
+
+
+    function createDog(url){
+        const newDog = document.createElement("img");
+        newDog.id = "doggie";
+        newDog.src = url;
+        document.querySelector("body").appendChild(newDog)
+    }
+
+    function fetchDog(){
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(res => res.json())
+        .then(data => createDog(data.message))
+    }
+    
+
+    // createDog(dogUrl)
+    fetchDog()
+
 })
